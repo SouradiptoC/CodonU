87d9481e05f87facc37948e341a567c2d19ac45e
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2022-09-14 22:37:14 +0530

    Updated FileHandler.py

--- srcs/FileHandler.py
+++ srcs/FileHandler.py
@@ -3,8 +3,11 @@ import os
 
 def make_dir(path):
     if not os.path.isdir(path):
-        print('hello')
         os.mkdir(path)
+
+
+def is_file(path):
+    return os.path.isfile(path)
 
 
 if __name__ == '__main__':
