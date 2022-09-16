fc6632526c276e7bdb29c40ccac9a719190cb9f4
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2022-09-16 15:32:42 +0530

    Updated FileHandler.py

--- srcs/FileHandler.py
+++ srcs/FileHandler.py
@@ -1,7 +1,8 @@
 import os
 from Bio.SeqIO import write
 from Bio.SeqRecord import SeqRecord
-from CdsExtractor import extract_cds
+from CdsExtractor import extract_cds, extract_prot
+from Errors import FileNotEmpty
 
 
 def make_dir(path: str) -> None:
@@ -22,17 +23,38 @@ def is_file(path: str) -> bool:
     return os.path.isfile(path)
 
 
-def write_fasta(file_name: str, cds_lst: list, record: SeqRecord) -> None:
+def is_file_empty(path: str) -> bool:
+    if os.stat(path).st_size == 0:
+        return True
+    else:
+        raise FileNotEmpty(path)
+
+
+def write_nucleotide_fasta(file_name: str, cds_lst: list, record: SeqRecord) -> None:
     """
-    Creates a fasta file if not exists previously
+    Creates a fasta file of nucleotides if not exists previously or is empty
     :param file_name: The name or path of the file
-    :param cds_lst: The list of FeatureLocation object
+    :param cds_lst: The list of FeatureLocation objects
     :param record: The SeqRecord object containing whole sequence
     """
-    if not is_file(file_name):
-        with open(file_name, 'a') as out_file:
+    if not is_file(file_name) or is_file_empty(file_name):
+        with open(file_name, 'w') as out_file:
             for i in range(len(cds_lst)):
                 cds = extract_cds(record, cds_lst[i], i + 1)
+                write(cds, out_file, 'fasta')
+
+
+def write_protein_fasta(file_name: str, cds_lst: list, organism_name: str) -> None:
+    """
+    Creates a fasta file of proteins if not exists previously or is empty
+    :param file_name: The name or path of the file
+    :param cds_lst: The list of FeatureLocation objects
+    :param organism_name: Name of the organism
+    """
+    if not is_file(file_name) or is_file_empty(file_name):
+        with open(file_name, 'w') as out_file:
+            for i in range(len(cds_lst)):
+                cds = extract_prot(cds_lst[i], organism_name, i + 1)
                 write(cds, out_file, 'fasta')
 
 
