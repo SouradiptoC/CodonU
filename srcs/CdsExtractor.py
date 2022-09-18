8f82aeada6703dc2b6af4d5075d3beb1c458fd2a
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2022-09-19 01:25:05 +0530

    Updated CdsExtractor.py

--- srcs/CdsExtractor.py
+++ srcs/CdsExtractor.py
@@ -3,6 +3,16 @@ from Bio.SeqFeature import FeatureLocation, SeqFeature
 from Bio.SeqRecord import SeqRecord
 
 
+def extract_cds_lst(record: SeqRecord) -> tuple:
+    """
+    Extracts the list of features if their type if CDS
+    :param record: Original Sequence Record object from where the CDS is to be extracted
+    :return: A tuple of FeatureLocation objects
+    """
+    cds_lst = [cds for cds in record.features if cds.type == 'CDS']
+    return tuple(cds_lst)
+
+
 def extract_cds_seq(seq: Seq, feature_location: FeatureLocation) -> Seq:
     """
     Extracts the CDS from a given sequence
