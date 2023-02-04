b0003edeb9b127e3867b6cebaae9ff843e054232
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-02-04 12:59:30 +0530

    Added color modification to enc

--- srcs/Vizualizer.py
+++ srcs/Vizualizer.py
@@ -27,16 +27,21 @@ def plot_enc(enc_val_lst: list, gc_val_lst: list, organism_name: None | str = No
     x = list(np.linspace(0, 1, 201))
     y = [_enc(i) for i in x]
     N = len(enc_val_lst)
+    color = enc_val_lst
+    plt.figure(figsize=(9, 5.25))
     plt.plot(x, y, color='red', label=r"$EN_c = 2 + s + \frac{29}{s^2 + (1 - s^2)}$")
-    plt.scatter(gc_val_lst, enc_val_lst, s=5, label=r"Measured $EN_c$ values")
+    plt.scatter(gc_val_lst, enc_val_lst, s=5, label=r"Measured $EN_c$ values", c=color, cmap='viridis')
     suptitle = r'$EN_c$ plot' if organism_name is None else f"$EN_c$ plot for {organism_name}"
     plt.suptitle(suptitle, fontsize=16)
     plt.title(f'Total genes: {N}', fontsize=12)
     plt.legend()
     plt.xlabel(r"$GC_3$ Value")
     plt.ylabel(r"$EN_c$ value")
+    c_bar = plt.colorbar()
+    c_bar.set_label(r'$EN_c values$')
     if save_image:
         name = 'ENc_plot.png' if organism_name is None else f"ENc_plot_{organism_name}.png"
         file_name = join(folder_path, name)
         plt.savefig(file_name, dpi=500)
     plt.show()
+    plt.close()
