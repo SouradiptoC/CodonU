e87db2afa54a643a5bc42a4247b34b91c3a9d44c
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-02-05 20:26:50 +0530

    Updated file name

--- srcs/cua_errors/__init__.py
+++ srcs/cua_errors/__init__.py
@@ -1,5 +1,5 @@
-from .file_not_empty_error import FileNotEmptyError
+from .file_not_empty_err import FileNotEmptyError
 from .no_prot_err import NoProteinError
-from .Nucleotide_err import NucleotideError
+from .nucleotide_err import NucleotideError
 from .internal_stop_codon_err import InternalStopCodonError
 from .ter_seq_err import TerSeqError
