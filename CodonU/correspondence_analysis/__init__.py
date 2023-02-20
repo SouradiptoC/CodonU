f3e9a60d22e65d527036e85598e5c2883afadfe1
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-02-20 18:26:55 +0530

    Updated files

---
+++ CodonU/correspondence_analysis/__init__.py
@@ -0,0 +1,1 @@
+from .mca_codon_freq import mca_codon_freq
--- CodonU/cua_warnings/__init__.py
+++ CodonU/cua_warnings/__init__.py
@@ -2,3 +2,4 @@ from .no_syn_codon_warn import NoSynonymousCodonWarning
 from .missing_codon_warn import MissingCodonWarning
 from .api_warn import ApiWarning
 from .email_warn import EmailWarning
+from .no_codon_warn import NoCodonWarning
