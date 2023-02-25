747c1b3889f937d26ce79c5ef4fd37cd507cb59c
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-02-25 22:48:47 +0530

    Added autoapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../CodonU'))
+
 project = 'CodonU'
 copyright = '2023, Souradipto Choudhuri'
 author = 'Souradipto Choudhuri'
@@ -14,12 +19,16 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "autoapi.extension",
+]
+
+autoapi_file_patterns = ['*.py']
+autoapi_dirs = ['../../CodonU']
+autoapi_type = 'python'
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
