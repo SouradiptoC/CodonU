a02d5dc6d43fed4beeb353049e6143f71e377548
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-03-05 17:45:51 +0530

    Added function call

--- CodonU/correspondence_analysis/__init__.py
+++ CodonU/correspondence_analysis/__init__.py
@@ -1,3 +1,5 @@
 from .mca_codon_freq import mca_codon_freq
-from .plot_mca_codon_freq import plot_mca_codon_freq
 from .mca_codon_rscu import mca_codon_rscu
+from .mca_aa_freq import mca_aa_freq
+from .plot_mca_codon_freq import plot_mca_codon_freq
+from .plot_mca_rscu import plot_mca_rscu
