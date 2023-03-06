7411c281da358acd66036a74b2b8d5a02d8321b7
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-03-06 18:03:46 +0530

    Added function call

--- CodonU/vizualizer/__init__.py
+++ CodonU/vizualizer/__init__.py
@@ -1,3 +1,7 @@
 from .plot_enc import plot_enc
 from .plot_pr2 import plot_pr2
 from .plot_neutrality import plot_neutrality
+from .plot_mca_aa_aroma import plot_mca_aa_aroma
+from .plot_mca_aa_gravy import plot_mca_aa_gravy
+from .plot_mca_codon_freq import plot_mca_codon_freq
+from .plot_mca_rscu import plot_mca_rscu
