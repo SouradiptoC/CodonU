359c6847abdcd102779092c460ed1fb61b2f8d31
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-03-07 00:56:42 +0530

    Updated function calls

--- CodonU/analyzer/__init__.py
+++ CodonU/analyzer/__init__.py
@@ -2,3 +2,5 @@ from .cai_comp import calculate_cai
 from .rscu_comp import calculate_rscu
 from .cbi_comp import calculate_cbi
 from .enc_comp import calculate_enc
+from .gravy_comp import calculate_gravy
+from .aromaticity_comp import calculate_aromaticity
