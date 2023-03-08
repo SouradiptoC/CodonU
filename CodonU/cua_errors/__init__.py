be4570f4fe877edc5fc9374d023d1ceedd7f6c44
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-03-09 00:29:09 +0530

    Added error call

--- CodonU/cua_errors/__init__.py
+++ CodonU/cua_errors/__init__.py
@@ -3,3 +3,4 @@ from .no_prot_err import NoProteinError
 from .nucleotide_err import NucleotideError
 from .internal_stop_codon_err import InternalStopCodonError
 from .ter_seq_err import TerSeqError
+from .unsupported_type import UnsupportedType
