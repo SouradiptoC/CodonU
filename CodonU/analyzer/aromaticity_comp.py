89b02722f57e0c9c118347c5c0fbed408d7ac17e
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-03-24 16:58:37 +0530

    provided sequence info

--- CodonU/analyzer/aromaticity_comp.py
+++ CodonU/analyzer/aromaticity_comp.py
@@ -8,7 +8,7 @@ def calculate_aromaticity(handle: str, min_len_threshold: int = 66, gene_analysi
     Calculates the aromaticity score for a given protein sequence
 
     :param handle: Handle to the file, or the filename as a string
-    :param min_len_threshold: Minimum length of nucleotide sequence to be considered as gene
+    :param min_len_threshold: Minimum length of protein sequence to be considered as gene
     :param gene_analysis: Option if gene analysis (True) or genome analysis (False) (optional)
     :return: The aromaticity score of given sequence if gene_analysis is false, else the dictionary containing gene number and corresponding GRAVY score
     """
--- CodonU/analyzer/gravy_comp.py
+++ CodonU/analyzer/gravy_comp.py
@@ -8,7 +8,7 @@ def calculate_gravy(handle: str, min_len_threshold: int = 66, gene_analysis: boo
     Calculates the gravy score for a given protein sequence
 
     :param handle: Handle to the file, or the filename as a string
-    :param min_len_threshold: Minimum length of nucleotide sequence to be considered as gene
+    :param min_len_threshold: Minimum length of protein sequence to be considered as gene
     :param gene_analysis: Option if gene analysis (True) or genome analysis (False) (optional)
     :return: The GRAVY score of given sequence if gene_analysis is false, else the dictionary containing gene number and corresponding GRAVY score
     """
