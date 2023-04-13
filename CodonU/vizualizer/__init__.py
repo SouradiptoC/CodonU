86e14a0ab2f854174d3dc054fd5518402096e1bf
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-04-13 19:28:13 +0530

    Added function calls

--- CodonU/vizualizer/__init__.py
+++ CodonU/vizualizer/__init__.py
@@ -5,3 +5,5 @@ from .plot_mca_aa_aroma import plot_mca_aa_aroma
 from .plot_mca_aa_gravy import plot_mca_aa_gravy
 from .plot_mca_codon_freq import plot_mca_codon_freq
 from .plot_mca_rscu import plot_mca_rscu
+from .plot_phy_dnd import plot_phy_dnd
+from .plot_phy_nex import plot_phy_nex
