fdec64a3704cc2f8432b927ba4c162f7dfe6dec7
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-04-24 12:40:52 +0530

    Added function calls

--- CodonU/cua_warnings/__init__.py
+++ CodonU/cua_warnings/__init__.py
@@ -1,5 +1,6 @@
 from .no_syn_codon_warn import NoSynonymousCodonWarning
 from .missing_codon_warn import MissingCodonWarning
+from .no_codon_warn import NoCodonWarning
 from .api_warn import ApiWarning
 from .email_warn import EmailWarning
 from .no_codon_warn import NoCodonWarning
--- CodonU/file_handler/__init__.py
+++ CodonU/file_handler/__init__.py
@@ -4,3 +4,4 @@ from .make_dir import make_dir
 from .read_file import read_file
 from .write_nucleotide_fasta import write_nucleotide_fasta
 from .write_protein_fasta import write_protein_fasta
+from .write_exome_fasta import write_exome_fasta
