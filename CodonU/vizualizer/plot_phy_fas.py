17e3d45dc81de3e5ec806d03ff95f6d684a3aaac
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-04-26 18:24:55 +0530

    Added function to draw phylogenetic tree from fasta file

---
+++ CodonU/vizualizer/plot_phy_fas.py
@@ -0,0 +1,39 @@
+from Bio.AlignIO import read
+from Bio.Phylo import draw
+from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
+from os.path import join, abspath
+import matplotlib.pyplot as plt
+from CodonU.file_handler import make_dir
+from CodonU.file_handler.internal_comp import is_file_writeable
+
+
+def plot_phy_fas(handle: str, title: str = 'Phylogenetic Tree', save_image: bool = False, folder_path: str = 'Report'):
+    """
+    Plots phylogenetic tree from fasta file
+    :param handle: Handle to the fasta file
+    :param title: Title of the plot
+    :param save_image: Options for saving the image (optional)
+    :param folder_path: Folder path where image should be saved (optional)
+    """
+    cons = DistanceTreeConstructor()
+    aln_file = read(open(handle), 'fasta')
+    calc = DistanceCalculator('identity')
+    distance_matrix = calc.get_distance(aln_file)
+    tree = cons.upgma(distance_matrix)
+    tree = tree.as_phyloxml()
+    fig, ax = plt.subplots()
+    fig.suptitle(title)
+    draw(tree, axes=ax)
+    if save_image:
+        make_dir(folder_path)
+        file_name = '_'.join(title.split()) + '_fas.png'
+        file_path = join(folder_path, file_name)
+        if is_file_writeable(file_path):
+            fig.savefig(file_path, dpi=500)
+            print(f'Saved file can be found as {abspath(file_path)}')
+    plt.close(fig)
+
+
+if __name__ == '__main__':
+    in_file = '/home/souro/Projects/CodonU/Examples/Report/phylo_input_test_aligned_o.fasta'
+    plot_phy_fas(in_file)
