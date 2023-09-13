950de650de5743f4323d471e16036547931d0e14
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-09-13 23:56:26 +0530

    Added call to CodonTableError nd BadSequenceError

--- CodonU/cua_errors/__init__.py
+++ CodonU/cua_errors/__init__.py
@@ -4,3 +4,5 @@ from .nucleotide_err import NucleotideError
 from .internal_stop_codon_err import InternalStopCodonError
 from .ter_seq_err import TerSeqError
 from .unsupported_type import UnsupportedType
+from .codon_table_exist_err import CodonTableExistsError
+from .bad_seq_err import BadSequenceError
