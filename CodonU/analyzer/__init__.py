f272545340f7f86dd7e4d1385ce504ac3d24b962
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-09-24 00:03:57 +0530

    Added function calls to tAI

--- CodonU/analyzer/__init__.py
+++ CodonU/analyzer/__init__.py
@@ -4,6 +4,7 @@ from .cbi_comp import calculate_cbi
 from .enc_comp import calculate_enc
 from .gravy_comp import calculate_gravy
 from .aromaticity_comp import calculate_aromaticity
+from .tRNA_comp import get_anticodon_count_dict, calculate_gtai
 from .generate_report_summary import generate_report_summary
 from .generate_report import generate_report
 from .internal_comp import *
