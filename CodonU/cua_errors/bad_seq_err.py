05aeb12b1dcce5e59fb996a61e3392d5de80f37c
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-09-27 23:18:06 +0530

    Updated BadSequenceError

--- CodonU/cua_errors/bad_seq_err.py
+++ CodonU/cua_errors/bad_seq_err.py
@@ -6,9 +6,13 @@ class BadSequenceError(CodonUsageError):
     Occurs when the sequence is bad i.e. length of the sequence is not divisible by 3
     """
 
-    def __init__(self, seq):
+    def __init__(self, seq, code):
         """
         :param seq: The nucleotide sequence which is bad
         """
-        self.msg = f"Length of the sequence is not divisible by 3. Check the sequence below.\n" \
-                   f"{seq}"
+        if code == 1:
+            self.msg = f"Length of the sequence is not divisible by 3. Check the sequence below.\n" \
+                       f"{seq}"
+        if code == 2:
+            self.msg = f"Length of the sequence is not divisible by 3. Check the sequence below.\n" \
+                       f"{seq.description}: {seq.seq}"
