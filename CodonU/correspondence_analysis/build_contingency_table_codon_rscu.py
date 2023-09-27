39809a84fe1d9c59ff01b860620c1f8bc2f310d4
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-09-27 23:19:45 +0530

    Added function build cont_table of codon RSCU

---
+++ CodonU/correspondence_analysis/build_contingency_table_codon_rscu.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from Bio.Data.CodonTable import unambiguous_dna_by_id
+from CodonU.analyzer import calculate_rscu
+from CodonU.file_handler import make_dir
+from CodonU.file_handler.internal_comp import is_file_writeable
+from os.path import join, abspath
+
+
+def build_contingency_table_codon_rscu(handle: str, genetic_table_num: int, min_len_threshold: int = 200,
+                                       save_file: bool = False, file_name: str = 'contingency_codon_rscu',
+                                       folder_path: str = 'Report') -> pd.DataFrame:
+    """
+    Creates the contingency table of codon RSCU\n
+    **Note** Gene descriptions are index headings and codons are column headings
+
+    :param handle: Handle to the file, or the filename as a string
+    :param genetic_table_num: Genetic table number for codon table
+    :param min_len_threshold: Minimum length of nucleotide sequence to be considered as gene
+    :param save_file: Option for saving the values in xlsx format (Optional)
+    :param file_name: Intended file name (Optional)
+    :param folder_path: Folder path where image should be saved (optional)
+    :return: The contingency table as a pandas DataFrame object
+    """
+    codons = list(unambiguous_dna_by_id[genetic_table_num].forward_table.keys())
+    rscu_dict = calculate_rscu(handle, genetic_table_num, min_len_threshold, gene_analysis=True)
+    genes = list(rscu_dict.keys())
+
+    contingency_table = pd.DataFrame(index=genes, columns=codons)
+    for gene in genes:
+        for codon in codons:
+            contingency_table[codon][gene] = rscu_dict[gene][codon]
+
+    if save_file:
+        name = file_name + '.xlsx'
+        make_dir(folder_path)
+        file_path = join(folder_path, name)
+        if is_file_writeable(file_path):
+            contingency_table.to_excel(file_path, float_format='%.5f', columns=contingency_table.columns)
+        print(f'The Contingency table of codon RSCU can be found at: {abspath(file_path)}')
+
+    return contingency_table
