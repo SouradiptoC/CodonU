90be3b5c4cbb440ede3b3a79161d68e1c4edb251
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2023-09-28 22:53:39 +0530

    Updated files

--- CodonU/extractor/extract_cds.py
+++ CodonU/extractor/extract_cds.py
@@ -1,21 +1,22 @@
-from Bio.SeqFeature import FeatureLocation
+from Bio.SeqFeature import SeqFeature
 from Bio.SeqRecord import SeqRecord
 
-from .internal_comp import extract_cds_seq
+from CodonU.extractor.internal_comp import extract_cds_seq
 
 
-def extract_cds(record: SeqRecord, feature_location: FeatureLocation) -> SeqRecord:
+def extract_cds(record: SeqRecord, cds_feature: SeqFeature) -> SeqRecord:
     """
     Returns the CDS as a Sequence Record object
 
     :param record: Original Sequence Record object from where the CDS is to be extracted
-    :param feature_location: The location of CDS
+    :param cds_feature: Sequence Feature object of corresponding SeqRecord object
     :return: The new Sequence Record object containing the CDS
     """
+    desc_lst = [f'[{key}={val}]' for key, val in cds_feature.qualifiers.items() if not key == 'translation']
     cds = SeqRecord(
-        seq=extract_cds_seq(record.seq, feature_location),
-        id=f"{record.id} {record.annotations['organism']}",
+        seq=extract_cds_seq(record.seq, cds_feature.location),
+        id=f"{record.id}|{record.annotations['organism']}",
         name=f"{record.annotations['organism']}",
-        description=f"{record.description}"
+        description=' '.join(desc_lst),
     )
     return cds
