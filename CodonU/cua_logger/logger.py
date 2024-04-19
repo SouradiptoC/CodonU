48cfe474da93fa078d806d54ccb91b2fecbae5c2
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2024-04-19 15:50:21 +0530

    Created console and timeRollingFileLogger

--- CodonU/cua_logger/logger.py
+++ CodonU/cua_logger/logger.py
@@ -1,29 +1,52 @@
-import logging
 import logging.config
-
 import yaml
+import os
 
-# path to config file
-logger_config_file = 'logger_config.yaml'
+# Creating logs dir
+if not os.path.isdir('logs'):
+    os.mkdir('logs')
 
+# Creating config file not existing
+# A new logfile will be generated at midnight
+# A total 7 log files will be there in 'logs' dir
+# 1 for the current run, 6 back up files
+if not os.path.isfile('CodonU_logger_config.yaml'):
+    config_yaml_dict = \
+        {'version': 1,
+         'formatters': {
+             'console': {'format': '%(asctime)s - %(levelname)s - %(message)s',
+                         'datefmt': '%Y-%m-%d %H:%M:%S'},
+             'file': {'format': '%(asctime)s - %(module)s - %(funcName)s - %(levelname)s - %(message)s',
+                      'datefmt': '%Y-%m-%d %H:%M:%S'}},
+         'handlers': {
+             'console':
+                 {'class': 'logging.StreamHandler',
+                  'level': 'INFO',
+                  'formatter': 'console',
+                  'stream': 'ext://sys.stdout'},
+             'rotating_file_handler':
+                 {'class': 'logging.handlers.TimedRotatingFileHandler',
+                  'level': 'INFO',
+                  'formatter': 'file',
+                  'filename': 'logs/CodonU_log',
+                  'when': 'midnight',
+                  'interval': 1,
+                  'backupCount': 6,
+                  'encoding': 'utf-8',
+                  'delay': True,
+                  }},
+         'loggers': {
+             'console_logger': {'handlers': ['console']},
+             'file_logger': {'handlers': ['rotating_file_handler'],
+                             'propagate': False}}}
+    with open('CodonU_logger_config.yaml', 'w') as wf:
+        yaml.dump(config_yaml_dict, wf, default_flow_style=False)
 
-def _create_config_dict(config_file: str) -> dict:
-    """
-    Creates config dict for logger
+# Reading data from CodonU_logger_config.yaml file
+with open('CodonU_logger_config.yaml', 'r') as f:
+    dic = yaml.safe_load(f)
+    logging.config.dictConfig(dic)
 
-    :param config_file: path to yaml file for config
-    :return: config dict
-    """
-    with open(config_file, "r") as config_file:
-        return yaml.safe_load(config_file.read())
-
-
-class CodonuLogger(object):
-    def __init__(self):
-        self.config_dict = _create_config_dict(logger_config_file)
-
-    def config_logger(self):
-        """
-        Configures the logger
-        """
-        logging.config.dictConfig(self.config_dict)
+# Creating the loggers
+file_log = logging.getLogger('file_logger')
+console_log = logging.getLogger('console_logger')
