0a6dc086c3b3bd790c7dc18257887090befd7df1
SouradiptoC <85365084+SouradiptoC@users.noreply.github.com>
2024-04-27 15:00:48 +0530

    Adding logger calls and automatic speed test

--- CodonU/file_handler/get_gb.py
+++ CodonU/file_handler/get_gb.py
@@ -1,6 +1,9 @@
 from Bio import Entrez
 from Bio.SeqIO import read
 from Bio.SeqRecord import SeqRecord
+from CodonU.cua_logger import *
+from CodonU.file_handler.network_speed import test_speed
+import sys
 
 
 def get_gb(accession_id: str) -> SeqRecord:
@@ -10,8 +13,16 @@ def get_gb(accession_id: str) -> SeqRecord:
     :param accession_id: Provided accession number
     :return: The Sequence Record object
     """
-    print(f"Retrieval started")
-    handle = Entrez.efetch(db='nucleotide', id=accession_id, rettype='gb', retmode='text')
-    record = read(handle, 'gb')
-    print(f"Genbank file of {record.annotations['source']} retrieved successfully")
-    return record
+    try:
+        test_speed()
+        console_log.info(f"Retrieval started")
+        file_log.info(f"Retrieval started")
+        handle = Entrez.efetch(db='nucleotide', id=accession_id, rettype='gb', retmode='text')
+        record = read(handle, 'gb')
+        console_log.info(f"Genbank file of {record.annotations['source']} retrieved successfully")
+        file_log.info(f"Genbank file of {record.annotations['source']} retrieved successfully")
+        return record
+    except Exception as e:
+        console_log.error(f'Following error occurred. See log files for details\n{e}')
+        file_log.exception(e)
+        sys.exit(-1)
